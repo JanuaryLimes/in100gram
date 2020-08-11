600d97915967a3b76faa8b7aaa735d9f63a4a9b2
kacper.sledz <casper.sledx@gmail.com>
2020-08-11 20:56:36 +0200

    insert test

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from 'apollo-server-micro'
+import { knex } from '../../knex'
 
 const typeDefs = gql`
   type Query {
@@ -7,12 +8,22 @@ const typeDefs = gql`
   type User {
     name: String
   }
+
+  type Mutation {
+    createPost(title: String!, description: String!): String
+  }
 `
 
 const resolvers = {
   Query: {
     users(_parent, _args, _context) {
       return [{ name: 'Next.js' }]
+    },
+  },
+  Mutation: {
+    createPost: async (_, { title, description }, _context) => {
+      await knex.insert({ title, description }).into('post').then()
+      return `post inserted: ${title} ${description}`
     },
   },
 }
