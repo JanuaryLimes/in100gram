dd3db98f8c7eb7971fa0d9e8dfc3abe32c7db65a
kacper.sledz <casper.sledx@gmail.com>
2020-08-23 13:46:19 +0200

    home link

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 
 export const Layout: React.FC = ({ children }) => {
   return (
     <div>
       <Head>
-        <title>In100gram - Instagram clone</title>
+        <title>In💯gram - Instagram clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <nav className="bg-white border-b flex justify-center top-0 sticky">
         <div className="flex w-full max-w-4xl p-4">
-          <span className="italic flex-auto">in100gram</span> {/* TODO link */}
+          <div className="italic flex-auto">
+            <Link href="/">
+              <button className="flex">
+                <div>In</div>
+                <div style={{ padding: '5px 2px 0 1px' }}>💯</div>
+                <div>gram</div>
+              </button>
+            </Link>
+          </div>
+          {/* TODO link */}
           <div className="flex-auto flex justify-center">search...</div>
           <div className="flex-auto flex justify-end">nav item...</div>
         </div>
