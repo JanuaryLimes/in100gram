9021308e212a6e0e8570d11bdc937ef0b6e14293
kacper.sledz <casper.sledx@gmail.com>
2020-08-23 13:46:55 +0200

    gap type purge test

--- components/Stack.tsx
+++ components/Stack.tsx
@@ -5,13 +5,21 @@ type StackProps = {
   gap?: 2 | 4
 }
 
+const gapType = {
+  2: 'mb-2',
+  4: 'mb-4',
+}
+
 export const Stack: React.FC<StackProps> = ({ components, gap = 4 }) => {
   return (
     <div>
       {components.map((component, index) => {
         const lastComponent = components.length - 1 == index
         return (
-          <div key={component.key} className={lastComponent ? '' : `mb-${gap}`}>
+          <div
+            key={component.key}
+            className={lastComponent ? '' : gapType[gap]}
+          >
             {component}
           </div>
         )
