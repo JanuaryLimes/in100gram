95ea94cfc675779c096ebf2c4a31414d60c6bc79
kacper.sledz <casper.sledx@gmail.com>
2020-08-25 17:46:45 +0200

    index page, login navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,43 @@
 import { Layout } from '../components/Layout'
 import { Main } from '../components/Main'
 import { posts } from '../utils/data'
+import { gql, useQuery } from '@apollo/client'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-export const Home = ({ postsData }): JSX.Element => (
-  <Layout>
-    <Main postsData={postsData} />
-  </Layout>
-)
+const ViewerQuery = gql`
+  query ViewerQuery {
+    viewer {
+      id
+      email
+    }
+  }
+`
+
+export const Home = ({ postsData }): JSX.Element => {
+  const router = useRouter()
+  const { data, loading, error } = useQuery(ViewerQuery)
+  const viewer = data?.viewer
+  const shouldRedirect = !(loading || error || viewer)
+
+  useEffect(() => {
+    if (shouldRedirect) {
+      router.push('/accounts/login')
+    }
+  }, [shouldRedirect])
+
+  // TODO error?
+
+  if (viewer) {
+    return (
+      <Layout>
+        <Main postsData={postsData} />
+      </Layout>
+    )
+  }
+
+  return <p>loading... [// TODO] </p>
+}
 
 Home.getInitialProps = async () => {
   return { postsData: posts }
