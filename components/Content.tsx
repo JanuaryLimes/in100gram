6d5991b74024d3a6fe9c7bccb49adfac4092d6d6
kacper.sledz <casper.sledx@gmail.com>
2020-11-05 21:26:51 +0100

    clear

--- components/Content.tsx
+++
@@ -1,7 +0,0 @@
-export const Content: React.FC = ({ children }) => {
-  return (
-    <div className="flex justify-center">
-      <div className="flex flex-col w-full max-w-2xl p-4">{children}</div>
-    </div>
-  )
-}
--- knex.ts
+++
@@ -1,6 +0,0 @@
-import Knex from 'knex'
-
-const environment = process.env.NODE_ENV || 'development'
-const config = require('./knexfile.js')[environment]
-
-export const knex = Knex(config)
